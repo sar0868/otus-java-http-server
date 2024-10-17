2900dd452cb196dbd5a94fbabdf5d3391b1fd3e0
sar0868 <safarov-ar@yandex.ru>
2024-10-17 14:45:56 +0300

    hw23: done

--- src/main/java/ru/otus/java/safarov/homework23/HttpServer.java
+++ src/main/java/ru/otus/java/safarov/homework23/HttpServer.java
@@ -3,6 +3,8 @@ package ru.otus.java.safarov.homework23;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private final int port;
@@ -15,20 +17,21 @@ public class HttpServer {
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ExecutorService executorService = Executors.newFixedThreadPool(4);
             while (true) {
                 Socket socket = serverSocket.accept();
                 byte[] buffer = new byte[8192];
                 int n = socket.getInputStream().read(buffer);
                 String rawRequest = new String(buffer, 0, n);
-                new Thread(() -> {
+                executorService.execute(() -> {
                     HttpRequest request = new HttpRequest(rawRequest);
-                    request.info(true);
+                    request.info(false);
                     try {
                         dispatcher.execute(request, socket.getOutputStream());
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-                }).start();
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
